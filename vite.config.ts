adba234221bec6609a71b57fc0e0cd91c0bef6e8
Alexia Giroud Nyer <alexia.giroudnyer@epfl.ch>
2025-12-06 20:15:48 +0100

    Deploy website

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/avADAkedavra25_website/',
+
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
