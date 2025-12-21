2997d8369b2420a164b97b8bbde0c2fd27c85a5c
Alexia Giroud Nyer <alexia.giroudnyer@epfl.ch>
2025-12-21 12:28:16 +0100

    feat: extend TypeScript declarations for image modules and JSX intrinsic elements

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,23 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
+
+declare module '*?url' {
+  const src: string;
+  export default src;
+}
+
 declare module 'figma:asset/*' {
   const src: string;
   export default src;
 }
 
 declare module 'react/jsx-runtime';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
